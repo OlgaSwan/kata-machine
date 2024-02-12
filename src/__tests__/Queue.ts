919cb86ee896e2d5f2dbd662f679fdc2c4f808bc
OlgaSwan <137110816+OlgaSwan@users.noreply.github.com>
2024-02-12 20:14:38 +0300

    implementing various algorithms and correcting incorrect test cases

--- src/__tests__/Queue.ts
+++ src/__tests__/Queue.ts
@@ -10,7 +10,7 @@ test("queue", function () {
     expect(list.deque()).toEqual(5);
     expect(list.length).toEqual(2);
 
-    // this must be wrong..?
+    // this must be wrong...?
     debugger;
 
     // i hate using debuggers
@@ -22,6 +22,8 @@ test("queue", function () {
     expect(list.deque()).toEqual(11);
     expect(list.deque()).toEqual(undefined);
     expect(list.length).toEqual(0);
+    expect(list.tail).toEqual(undefined);
+    expect(list.head).toEqual(undefined);
 
     // just wanted to make sure that I could not blow up myself when i remove
     // everything
