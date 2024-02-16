b926732aa4dfd37483293bcad2623a1ae74147c0
OlgaSwan <137110816+OlgaSwan@users.noreply.github.com>
2024-02-16 23:36:41 +0300

    implementing various traversals

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,12 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+const walk = (curr: BinaryNode<number> | null, path: number[]): number[] => {
+  if(!curr) return path
 
+  walk(curr.left, path)
+  walk(curr.right, path)
+  path.push(curr.value)
+
+  return path
+}
+export default function in_post_order(head: BinaryNode<number>): number[] {
+  return walk(head, [])
 }
